cd1dbbad8d2d29b8016af3ab507e1c8f6a96ae18
Alexander Bjerkan <alexander.bjerkan@obos.no>
2025-01-25 19:54:33 +0100

    formatphonenumber

--- packages/format/src/no.ts
+++ packages/format/src/no.ts
@@ -1,5 +1,25 @@
 import { replaceIfMatch } from './utils';
 
+// See https://sprakradet.no/godt-og-korrekt-sprak/rettskriving-og-grammatikk/tall-tid-dato/
+const REGULAR_PHONE_NUMBER_FORMAT = /^(\d{2})(\d{2})(\d{2})(\d{2})$/;
+const EIGHT_HUNDRED_SERIES_PHONE_NUMBER_FORMAT = /^(\d{3})(\d{2})(\d{3})$/;
+
+/**
+ * Format a phone number
+ * @example
+ * ```
+ * formatPhoneNumber('00000000') // => '00 00 00 00'
+ * formatPhoneNumber('80000000') // => '800 00 000'
+ * ```
+ */
+export function formatPhoneNumber(number: string): string {
+  if (number.startsWith('8')) {
+    return number.replace(EIGHT_HUNDRED_SERIES_PHONE_NUMBER_FORMAT, '$1 $2 $3');
+  }
+
+  return number.replace(DEFAULT_PHONE_NUMBER_FORMAT, '$1 $2 $3 $4');
+}
+
 const ORG_NUMBER_FORMAT = /^(\d{3})(\d{3})(\d{3})$/;
 
 /**
