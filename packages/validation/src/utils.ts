7dcc35bccfc6e1b6347071ef8557b841cb7629d8
Alexander Bjerkan <alexander.bjerkan@obos.no>
2025-02-26 06:27:37 +0100

    add Swedish validateNationalIdenityNumber()

--- packages/validation/src/utils.ts
+++ packages/validation/src/utils.ts
@@ -26,3 +26,49 @@ export function mod11(value: string, weights: number[]): boolean {
 
   return controlNumber === Number(value[value.length - 1]);
 }
+
+/**
+ * Also known as Luhn's algorithm.
+ * Used to validate Swedish national identity numbers.
+ * See https://no.wikipedia.org/wiki/MOD10
+ */
+export function mod10(value: string): boolean {
+  let sum = 0;
+
+  for (let i = 0; i < value.length; ++i) {
+    const weight = 2 - (i % 2);
+
+    let number = Number(value[i]);
+
+    number = weight * number;
+
+    // if the number is greater than 9, ie more than one digit, we reduce it to a single digit by adding the individual digits together
+    // 7 * 2 => 14 => 1 + 4 => 5
+    // instead of adding the digits together, we can subtract 9 for the same result
+    // 7 * 2 => 14 => 14 - 9 => 5
+    if (number > 9) {
+      number = number - 9;
+    }
+
+    sum += number;
+  }
+
+  return sum % 10 === 0;
+}
+
+export function isValidDate(year: number, month: number, day: number): boolean {
+  // months are zero indexed 🤷‍♂️
+  month -= 1;
+
+  // important to use UTC so the user's timezone doesn't affect the validation
+  const date = new Date(Date.UTC(year, month, day));
+
+  return (
+    date &&
+    // cannot do this for Norway
+    // maybe do it for Sweden?
+    // date.getUTCFullYear() === year &&
+    date.getUTCMonth() === month &&
+    date.getUTCDate() === day
+  );
+}
